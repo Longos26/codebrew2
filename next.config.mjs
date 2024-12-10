d3f96abfe91a27cc9cae185488ffe1471d8918fc
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-12-10 05:40:30 -0800

    Initial commit4

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    
+    output: 'export', // for server-side deployments
+    images:{
+        remotePatterns:[
+            {
+                protocol: 'https',
+                hostname: '*.googleusercontent.com',
+
+            },
+
+            {
+                protocol:'https',
+                hostname: 'tealerinpos.s3.amazonaws.com',
+            },
+        ]
+    }
+    
+};
 
 export default nextConfig;
